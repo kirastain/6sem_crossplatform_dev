fe16aee23648eb1365a83936985c2dd8b733e920
Daria Fedorova <36547686+kirastain@users.noreply.github.com>
2020-04-17 22:00:35 +0300

    authorization done
    kinda

---
+++ FDA_tech_lab_2/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FDA_tech_lab_2.Models;
+
+
+namespace FDA_tech_lab_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("token")]
+        public string GetToken()
+        {
+            return Authorization.GenerateToken();
+        }
+        [HttpGet("token/secret")]
+        public string GetAdminToken()
+        {
+            return Authorization.GenerateToken(true);
+        }
+    }
+}
--- FDA_tech_lab_2/Models/Authorization.cs
+++ FDA_tech_lab_2/Models/Authorization.cs
@@ -15,5 +15,28 @@ namespace FDA_tech_lab_2.Models
         public static string Audience => "LNGwannaknows";
         public static int LifetimeInYears => 2;
         public static SecurityKey SigningKey => new SymmetricSecurityKey(Encoding.ASCII.GetBytes("NothingIsTrueEverythingIsPermitted"));
+
+        internal static string GenerateToken(bool is_admin = false)
+        {
+            var now = DateTime.UtcNow;
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, "user"),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, is_admin?"admin":"guest")
+                };
+            ClaimsIdentity identity =
+            new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                    issuer: Issuer,
+                    audience: Audience,
+                    notBefore: now,
+                    expires: now.AddYears(LifetimeInYears),
+                    claims: identity.Claims,
+                    signingCredentials: new SigningCredentials(SigningKey, SecurityAlgorithms.HmacSha256)); ;
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+            return encodedJwt;
+        }
     }
 }
